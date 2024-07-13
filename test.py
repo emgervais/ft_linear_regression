e0a4e94234b8708c21ab3fed4beaa9ad60b37bb0
emgervais <129791447+emgervais@users.noreply.github.com>
2024-07-12 22:03:22 -0400

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from src.estimate import norm, denorm 
-# import '/src/estimate.py' as e
-
-ok = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
-
-n = norm(ok, 9)
-r = denorm(ok , n)
-print(r)
